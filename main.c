0d07b9b33f378a39adb278c26cfbe4275e486d0e
Iliass Talea <166926284+taleailiass@users.noreply.github.com>
2025-12-17 16:08:14 +0100

    Fix formatting and add difficulty level options

--- main.c
+++ main.c
@@ -11,3 +11,8 @@ char playAgain;
 srand (time(0));
 printf("Welcome to Guess the Number Game ");
 do { printf("\n choose difficulty Level: \n"); 
+printf("1.Easy (1-50,10 attempts)\n");
+printf("2.Medium((1-100,7 attempts)\n");
+printf("3.Hard (1-200, 5 attempts)\m");
+printf("Enter 1-3: ");
+clearbuffer();
