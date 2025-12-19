ea9a036758b42ed5be5419c760b5d7dd79470f65
Iliass Talea <166926284+taleailiass@users.noreply.github.com>
2025-12-19 18:24:02 +0100

    Correct syntax and logic in guessing game
    Fix syntax errors and improve guessing game logic.

--- main.c
+++ main.c
@@ -19,3 +19,25 @@ clearbuffer();
 switch (level) {
 case 1: number = rand() %50+1 ; maxAttempts=10; break;
 case 2; number = rand() %100+1; maxAttempts=7; break;
+case 3:number =rend() %200+1: maxAttempts=5; break;
+default : number =rand() %100+1; maxAttempts=5; break;
+}
+printf("\nI Have picked a number . Try to guess it \n");
+atttempts=0;
+do{
+printf("Enter your guess : ");
+scanf("%d, &guess");
+clearbuffer();
+attempts++;
+if(guess< number) printf("Higher\n"){
+else if (guess>number) printf("Lower\n")
+else {printf("Correct ! you goty it  in %d attempts\n ",attempts);
+     score+=(maxAtteeempts-attempts+1)*10;//Add points
+break;}
+if (attempts==maxAttempts){
+printf("Out of Atteempts! the number was %d.\n",number);}
+else if ((level ==1 && attempts==maxAttempts/2) || (level==2 && attempts == maxAttempts/2)) {
+printf("Hint: The number is %s than %d\n, guess <number ? "greater":"smaller",guess);
+  }
+}
+while ()
